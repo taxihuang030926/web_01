f6e135e42af5858c9b9456b2e288c60a03b0e7fe
Tim Huang <taxihuang030926@gmail.com>
2022-03-14 16:38:29 +0800

    add some presets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <p class="default">Hello Tim</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +17,17 @@ export default {
   }
 }
 </script>
+
+<style lang='scss' scoped>
+.default{
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height:100vh;
+  color:black;
+  font-size: 12px;
+  font-weight: 900px;
+}
+
+</style>
