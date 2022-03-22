3d1ff64401f95cd0074509b7739deb7c8cc0bbe9
Tim Huang <taxihuang030926@gmail.com>
2022-03-22 15:41:52 +0800

    add some pictures, contents in home, modify some settings of navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,21 @@
 <template>
-  <div class="home">
-  </div>
+  <section id ="home">
+    <div class="home_section1">
+      <img class="title_img" src="../pictures/home/home.jpeg">
+    </div>
+  </section>  
+    
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/header.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    Header
-}
 }
 </script>
 
 <style lang='scss' scoped>
-.default{
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height:100vh;
-  color:black;
-  font-size: 12px;
-  font-weight: 900px;
-}
-
+@import "@/assets/scss/home.scss";
+@import "@/assets/scss/index.scss";
 </style>
