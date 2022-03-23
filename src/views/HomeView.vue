2f37fef7f5e6c98f212956a7b6a757d9c69a061c
Tim Huang <taxihuang030926@gmail.com>
2022-03-23 10:08:03 +0800

    add sth to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,18 @@
 <template>
   <section id ="home">
-    <div class="home_section1">
+    <div id="home_section1" class="section" style="background-color: #1a1a1a">
       <img class="title_img" src="../pictures/home/home.jpeg">
     </div>
+    <div id="home_section2" class="section" style="background-color: #1a1a1a">
+      <div id="content1" class="content">
+        <h1>こんにちは、わたしは<br><h2>Tim HUANG</h2></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Hic, nesciunt dolorem veritatis, culpa dolores officia alias molestiae quaerat placeat 
+          qui incidunt aperiam vero enim vel sint veniam ipsa? Commodi, veniam?
+          </p>
+      </div>
+    </div>
+    
   </section>  
     
 </template>
