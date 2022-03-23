83b7f4d0df026166fedef595675b84afcaf001db
Tim Huang <taxihuang030926@gmail.com>
2022-03-23 16:50:39 +0800

    add some contents to home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,12 +5,19 @@
     </div>
     <div id="home_section2" class="section" style="background-color: #1a1a1a">
       <div id="content1" class="content">
-        <h1>こんにちは、わたしは<br><h2>Tim HUANG</h2></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Hic, nesciunt dolorem veritatis, culpa dolores officia alias molestiae quaerat placeat 
-          qui incidunt aperiam vero enim vel sint veniam ipsa? Commodi, veniam?
-          </p>
+        <h1>こんにちは<br><h2>Tim HUANG</h2></h1>
+        <p style="color: white"> 
+          Here will be some brief introduction to myself.
+        </p>
       </div>
+    </div>
+    <div id="home_section3" class="section" style="background-color: #1a1a1a">
+      <div id="content2" class="content">
+        <h1>Gallery</h1>
+        <p style="color: white">
+          Here will be a gallery of photos I took and videos I made
+        </p>
+      </div> 
     </div>
     
   </section>  
